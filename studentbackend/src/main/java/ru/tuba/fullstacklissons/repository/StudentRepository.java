11a3a1ad65ed877d7159241561f733a6f4a2e42a
Tubach1 <140211809+Tubach1@users.noreply.github.com>
2023-10-19 23:35:04 +0300

    Delete studentbackend directory

--- studentbackend/src/main/java/ru/tuba/fullstacklissons/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.tuba.fullstacklissons.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.tuba.fullstacklissons.model.Student;
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
